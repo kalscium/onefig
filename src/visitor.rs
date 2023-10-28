bf44ef0ae1c258cda3576905345cfbe4acd3cdf9
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-10-28 19:16:36 +1100

    Changed confhashmap into a trait for the hashmap rather than a struct

--- src/visitor.rs
+++ src/visitor.rs
@@ -3,7 +3,9 @@ use hashbrown::HashMap;
 use crate::errors::LogicError;
 
 pub type ConfHashMap = HashMap<Box<str>, (Position, Value)>;
-pub struct ConfTable(ConfHashMap);
+pub trait ConfTable {
+    fn set(&mut self, path: &[Box<str>], value: Value, pos: Position);
+}
 
 pub struct Config {
     pub position: Position,
@@ -17,36 +19,31 @@ pub enum Value {
     Path(Box<[Box<str>]>),
     Bool(bool),
     List(Box<[(Position, Value)]>),
-    Table(ConfTable),
+    Table(ConfHashMap),
     Raw(Box<str>),
 }
 
 pub trait VisitValue {
-    fn visit(self, map: &mut ConfTable, scope: Box<[Box<str>]>);
+    fn visit(self, map: &mut ConfHashMap, scope: Box<[Box<str>]>);
 }
 
 pub trait VisitConfig {
-    fn visit(self, map: &mut ConfTable, scope: Box<[Box<str>]>) -> Config;
+    fn visit(self, map: &mut ConfHashMap, scope: Box<[Box<str>]>) -> Config;
 }
 
-impl ConfTable {
-    #[inline]
-    pub fn new() -> Self {
-        Self(HashMap::new())
-    }
-
-    pub fn set(&mut self, path: &[Box<str>], value: Value, pos: Position) {
+impl ConfTable for ConfHashMap {
+    fn set(&mut self, path: &[Box<str>], value: Value, pos: Position) {
         if path.len() == 1 {
-            if let Some((first, _)) = self.0.insert(path[0].clone(), (pos.clone(), value)) {
+            if let Some((first, _)) = self.insert(path[0].clone(), (pos.clone(), value)) {
                 flexar::compiler_error!((LG001, pos) path[0], first.0.ln).throw()
             } return;
         }
 
-        match self.0.get_mut(&path[0]) {
+        match self.get_mut(&path[0]) {
             Some((_, Value::Table(x))) => x.set(&path[1..], value, pos),
             Some((first, _)) => flexar::compiler_error!((LG001, pos) path[0], first.0.ln).throw(),
             None => {
-                self.0.insert(path[0].clone(), (pos.clone(), Value::Table(ConfTable::new())));
+                self.insert(path[0].clone(), (pos.clone(), Value::Table(ConfHashMap::new())));
                 self.set(path, value, pos);
             },
         }
