9166af496245c09f45a4f8b9855fad9b9cc4c4eb
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-11-06 15:04:31 +1100

    for working on the language (temp)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use flexar::lext::Lext;
 use onefig::{lexer::Token, nodes::source_file::SourceFile, conff::ConfFile, cli::Cli};
 
-fn _old_main() {
+fn main() {
     let file = fs::read_to_string("example.nf").unwrap();
     let tokens = Token::tokenize(Lext::new("example.nf".into(), &file));
 
@@ -19,6 +19,6 @@ fn _old_main() {
     conf_files.iter().for_each(|x| x.generate());
 }
 
-fn main() {
+fn _new_main() {
     let _args = Cli::parse();
 }
